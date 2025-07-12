0b0846f3326f77d1b8bdafe60d1c6d04e9ae1d53
Anubhab2003 <anubhabchowdhury01@gmail.com>
2025-07-12 17:02:47 +0530

    cleaned file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+
 import NavBar from "./components/NavBar";
 import ToolBar from "./components/ToolBar";
 import Table from "./components/Table";
-import Header from "./components/Header";
+
 function App() {
   return (
     <>
       <div>
         <NavBar/>
         <ToolBar/>
-        {/* <Header/> */}
         <Table/>
       </div>
     </>
